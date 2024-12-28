665cc18e3e40a4c895283d5af72c837c9c5432ac
Brooke Joseph <brookecoco123123@gmail.com>
2024-12-27 19:35:05 -0500

    first commit

---
+++ dec_to_binary.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+int main(int argc, char *argv[])
+{
+    stringstream number;
+    int value;
+    number >> value;
+    while (value / 2 != 0)
+    {
+        if (value % 2 == 0)
+        {
+        }
+    }
+    return 0;
+}
